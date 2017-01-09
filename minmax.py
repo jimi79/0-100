800bb2dff42c8f2511d07c02b558d817a7dead83
jimi <jimi@io>
2017-01-10 00:06:13 +0100

    seems to work already

--- minmax.py
+++ minmax.py
@@ -23,21 +23,33 @@ class AI:
 	def __init__(self, dbname):
 		self.conn=sqlite3.connect(dbname, isolation_level=None)
 		self.cur=self.conn.cursor()
+		self.verbose=False
 
 	def init_db(self):
-		self.cur.execute('create table my_states(state varchar(20), maxmin integer, best_maxmin varchar(20));')
+		self.cur.execute('create table my_states(state integer, maxmin integer, action integer);')
 		self.cur.execute('create unique index idx_my_states on my_states(state)');
-		self.cur.execute('create table opp_states(state varchar(20), minmax integer, best_minmax varchar(20));')
+		self.cur.execute('create table opp_states(state integer, minmax integer, action integer);')
 		self.cur.execute('create unique index idx_opp_states on opp_states(state)');
-		self.cur.execute('create table lt(old_state varchar(20), action action varchar(20), new_state varchar(20), opponent boolean);')
+		self.cur.execute('create table lt(old_state integer, action action integer, new_state integer, opponent boolean);')
 		self.cur.execute('create unique index idx_lt_old_state on lt(old_state, opponent, action)');
 		self.cur.execute('create unique index idx_lt_new_state on lt(new_state, opponent, action)');
 
-	def play(self, state): 
+	def play(self, state, actions): 
 		# find best action with max output 
+		#action=random.randrange(0,10)+1
+		d=self.cur.execute("select action, maxmin from my_states where state=?", (state,)).fetchone()
+		if d[0] is None:
+			if self.verbose:
+				print("Picked an action at random")
+			action=random.choice(actions)
+		else:
+			action=d[0]
+			if d[1] is None:
+				if self.verbose:
+					print("Picked an action to test somethg new") 
 		return action
 
-	def init_state(self, state, actions, opponent=False):
+	def init_state(self, state, actions, opponent):
 		if opponent:
 			tablename='opp_states'
 		else:
@@ -50,107 +62,81 @@ class AI:
 				self.cur.execute('insert into %s(state) values (?);' % tablename, (state, ))
 				self.cur.executemany('insert into lt(old_state, action, opponent) values (?, ?, ?)', [(state, action, opponent) for action in actions]) 
 				self.cur.execute('commit') 
-				print("added")
 			except sqlite3.Error:
 				self.cur.execute("rollback")
 				raise
-		else:
-			print("already here")
 
-	def init_path(self, old_state, action, new_state, opponent=False):
+	def init_path(self, old_state, action, new_state, opponent):
 		self.cur.execute('update lt set new_state=? where old_state=? and action=? and opponent=?;', (new_state, old_state, action, opponent))
-		print("lt updated")
+
+	def init_points(self, state, points, opponent):
+		if opponent:
+			tablename='opp_states'
+			col='minmax'
+		else:
+			tablename='my_states' 
+			col='maxmin'
+		self.cur.execute('update %s set %s=? where state=?;' % (tablename, col), (points, state))
+
+	def calculate(self, state, opponent):
+		# we calculate ourself, and the the parents
+		if self.verbose:
+			print("Calculating state %d for %s" % (state, ['myself', 'opponent'][opponent]))
+		if opponent==True: # that is the opponent turn, so i check the minmax (it will try to minimize my points) 
+			table="opp_states"
+			other_table="my_states"
+			mm="minmax"
+			other_mm="maxmin"
+			desc=""
+		else: 
+			table="my_states"
+			other_table="opp_states"
+			mm="maxmin"
+			other_mm="minmax"
+			desc="desc"
 
 
-	def learn_points(self, state, points):
-		status=array_to_integer(status)
-		s=self.statuses.get(status)
-		if s is None:
-			s=Status()
-			self.statuses[status]=s
-		s.minmax=points
-		s.maxmin=points
-		s.minmax_action=None 
-		s.maxmin_action=None
 
-	def calculate(self, id_, lvl=99):
-		if lvl>0:
-			default=0 # i consider that the min the other can do is 0
-			s=self.statuses.get(id_)
-			if s is not None:
-				if self.verbose:
-					print("Calculate %d" % id_)
-				l=[]
-				for i in s.lto.items(): # i need to take the max of it, so i'll update maxmin
-					act=i[0]
-					s2=self.statuses.get(i[1])
-					if s2 is not None:
-						self.calculate(i[1], lvl-1)
-						if s2.maxmin is None:
-							l.append((default, act))
-						else:
-							l.append((s2.maxmin, act))
-					else:
-						l.append((default, act))
-				if len(l)>0:
-					l=sorted(l)
-					#if self.verbose:
-					#	print("minmax list %s" % l)
-					s.minmax_action=l[0][1]
-					s.minmax=l[0][0] 
-				l=[]
-				default=2
-				for i in s.lt.items(): # i need to take the max of it, so i'll update maxmin
-					act=i[0]
-					s2=self.statuses.get(i[1])
-					if s2 is not None:
-						self.calculate(i[1], lvl-1)
-						if s2.minmax is None:
-							l.append((default, act)) 
-						else:
-							l.append((s2.minmax, act))
-					else:
-						l.append((default, act))
-				if len(l)>0:
-					l=sorted(l, reverse=True)
-					#if self.verbose:
-					#	print("maxmin list %s" % l)
-					s.maxmin_action=l[0][1]
-					s.maxmin=l[0][0] 
-				l=[]
+# to follow unknown paths, just check lt that leads to somethg i don't know
+
 		
-	def print_tree_minmax(self, id_, action=-1, shift='', level_down=4): 
-		res=[]
-		if len(shift)>60:
-			raise Exception("infinite loop")
-		if id_ is None:
-			res.append("%s\\ %d->??" % (shift, action))
-		else:
-			s=self.statuses.get(id_)
-			if s is not None:
-				res.append("%s\ %d->%d (min=%s, action=%s)" % (shift, action, id_, s.minmax, s.minmax_action))	
-				level_down-=1
-				if level_down==0:
-					res.append("%s    \\..." % shift)
-				else:
-					for i in sorted(s.lto.items(), reverse=True):
-						res+=self.print_tree_maxmin(i[1], i[0], shift+'    ', level_down)
-			return res
+		action=None
+		d=self.cur.execute("select action from lt where old_state=? and new_state is null;", (state,)).fetchone()
+		if d is not None: # at least one record
+			action=d[0]
+		# if there is no result, perhaps we got a mm that is null (would be a repercution of an unknwon path
+		d=self.cur.execute("select lt.action from lt inner join %s as s on lt.new_state=s.state where old_state=? and s.%s is null;" % (other_table, other_mm), (state,)).fetchone()
+		if d is not None: # at least one record
+			action=d[0]
 
-	def print_tree_maxmin(self, id_, action=-1, shift='', level_down=4): 
-		res=[]
-		if len(shift)>60:
-			raise Exception("infinite loop")
-		if id_ is None:
-			res.append("%s\ %d->??" % (shift, action))
+		if action is not None: 
+			sql="update opp_states set action=? where state=?" # we don't know its outcome, so we store it as being the best
+			self.cur.execute(sql, (action, state))
+			if self.verbose:
+				print("u opp action=%d, state=%d" % (action, state))
+		else: 
+			sql="select lt.action, s.%s as mm from %s s inner join lt on lt.new_state=s.state and lt.opponent=1 where lt.old_state=? and not %s is null order by %s %s limit 1;" % (other_mm, other_table, other_mm, other_mm, desc)
+			d=self.cur.execute(sql, (state,)).fetchone()
+			if d is not None:
+				action=d[0]
+				mm_val=d[1] 
+				sql="update %s set %s=?, action=? where state=?" % (table, mm)
+				self.cur.execute(sql, (mm_val, action, state))
+				if self.verbose:
+					print("u %s minmax=%d, action=%d, state=%d" % (table, mm_val, action, state))
+
+	def recurse_calculate(self, state, opponent=None): # recurse from the end. 
+# recurse should create a list and send it back, then we sort/uniq it, and calculate it. but what for, because in any cases we know we have to recalculate everythg anyway, or at least every stuff below anythg that changes. might as well wait till the end and recalculate
+		if opponent==None:
+			self.recurse_calculate(state, True)
+			self.recurse_calculate(state, False)
 		else:
-			s=self.statuses.get(id_)
-			if s is not None:
-				res.append("%s\ %d->%d (max=%s, action=%s)" % (shift, action, id_, s.maxmin, s.maxmin_action))
-				level_down-=1
-				if level_down==0:
-					res.append("%s    \\..." % shift)
-				else:
-					for i in sorted(s.lt.items()):
-						res+=self.print_tree_minmax(i[1], i[0], shift+'    ', level_down) 
-		return res
+			self.calculate(state, opponent)
+# now we check on what depends that state
+			datas=self.cur.execute("select lt.old_state from lt where new_state=? and opponent=?", (state, not opponent)).fetchall()
+			for data in datas:
+				self.recurse_calculate(data[0], not opponent)
+
+	def is_over(self):
+		d=self.cur.execute('select count(1) from lt where new_state is null;').fetchone()[0]
+		return d==0
