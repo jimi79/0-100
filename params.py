01efe4881b6733301f65ffb5f7de751c057a0723
jimi <jimi@io>
2017-01-12 18:06:22 +0100

    missing file

---
+++ params.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+const_win=200
+const_actions=list(range(1,22)) # from 1 to 11 -1
+const_actions.remove(5) 
+const_actions.remove(2) 
+const_actions.remove(3) 
+const_actions.remove(13) 
+db='/tmp/ai.db' 
